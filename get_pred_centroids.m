d9c3f73271490beff217eab11bd5e645fbc01da4
Unknown <maimon.rose@gmail.com>
2020-09-09 15:39:35 -0500

    switched dimensions of centroid matrix

--- get_pred_centroids.m
+++ get_pred_centroids.m
@@ -13,14 +13,14 @@ color_names = color_pred_model.ClassificationSVM.ClassNames;
 nColor = length(color_names);
 
 nFrames = length(LEDTracks.centroidLocs);
-pred_centroids = nan(nFrames,nColor,2);
+pred_centroids = nan(nFrames,2,nColor);
 for frame_k = 1:nFrames
     for color_k = 1:nColor
         current_color_idx = strcmp(LEDTracks.predColors{frame_k},color_names{color_k}); % which color relative to the list used in the prediction model are we looking at?
         if sum(current_color_idx) > 1 % if the model predicted more than 1 of the same color, decide which to use
-            current_pred_posteriors = LEDTracks.predPosteriors{frame_k}(current_color_idx,color_k);
+            current_pred_posteriors = LEDTracks.predPosterior{frame_k}(current_color_idx,color_k);
             if length(unique(current_pred_posteriors)) == 1 % if multiple locations have the same posterior (e.g. both at maximum posterior), we can't decide between them, discard
-                pred_centroids(frame_k,color_k,:) = NaN;
+                pred_centroids(frame_k,:,color_k) = NaN;
                 continue
             else
                 [~,pred_posterior_idx] = max(current_pred_posteriors); % take the prediction with the higher posterior as the correct prediction
@@ -28,9 +28,9 @@ for frame_k = 1:nFrames
                 current_color_idx = current_color_idx(pred_posterior_idx);
             end
         elseif sum(current_color_idx) == 0 % if this color isn't present in the predictin mark as NaN
-            pred_centroids(frame_k,color_k,:) = NaN;
+            pred_centroids(frame_k,:,color_k) = NaN;
             continue
         end
-        pred_centroids(frame_k,color_k,:) = LEDTracks.centroidLocs{frame_k}(current_color_idx,:);
+        pred_centroids(frame_k,:,color_k) = LEDTracks.centroidLocs{frame_k}(current_color_idx,:);
     end
 end
