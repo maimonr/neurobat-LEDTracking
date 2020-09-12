a95c024a9b7ea6b43d3735e94bc92539a7cac586
BoazStyr <61527144+BoazStyr@users.noreply.github.com>
2020-09-12 11:42:52 -0700

    fixed bug in collecting variables in struct

--- batch_process_LED_tracking.m
+++ batch_process_LED_tracking.m
@@ -10,15 +10,18 @@ t2 = zeros(1,nVideo);
 parfor video_k = 1:nVideo
     v = VideoReader(fullfile(video_fNames(video_k).folder,video_fNames(video_k).name)); %#ok<TNMLP>
     t = tic;
-    [centroidLocs{video_k}, predColors{video_k}, props{video_k}, predPosterior{video_k}] = deal(cell(1,NumFrames ));
-    file_frame_number{video_k} = 1:v.NumFrames;
-    fileIdx{video_k} = video_k*ones(1,v.NumFrames);
-    for frame_k = 1: v.NumFrames
+    nFrame = v.NumFrames;
+    [centroidLocs{video_k}, predColors{video_k}, props{video_k}, predPosterior{video_k}, predLab{video_k}] = deal(cell(1,nFrame));
+    file_frame_number{video_k} = 1:nFrame;
+    fileIdx{video_k} = video_k*ones(1,nFrame);
+    for frame_k = 1:nFrame
         f = readFrame(v);
         if sum(f,'all') < lightsTh
             f = undistortFisheyeImage(f, cameraParams.Intrinsics);
             [centroidLocs{video_k}{frame_k}, predColors{video_k}{frame_k}, props{video_k}{frame_k}, predPosterior{video_k}{frame_k},predLab{video_k}{frame_k}] =...
                 predict_LED_location(f,'color_pred_model',color_pred_model,'params',LEDtrackingParams);
+        else
+            [centroidLocs{video_k}{frame_k}, predColors{video_k}{frame_k}, props{video_k}{frame_k}, predPosterior{video_k}{frame_k},predLab{video_k}{frame_k}] = deal(NaN);
         end
     end
     t2(video_k) = toc(t);
@@ -33,8 +36,8 @@ predLab = [predLab{:}];
 file_frame_number = [file_frame_number{:}];
 fileIdx = [fileIdx{:}];
 
-LEDTracks = struct('centroidLocs', centroidLocs, 'predColors', predColors,...
-    'props', props, 'predPosterior', predPosterior, 'predLab', predLab,...
+LEDTracks = struct('centroidLocs', {centroidLocs}, 'predColors', {predColors},...
+    'props', {props}, 'predPosterior', {predPosterior}, 'predLab', {predLab},...
     'video_fNames', video_fNames, 'cameraParams', cameraParams,...
     'color_pred_model', color_pred_model,'lightsTh', lightsTh,...
     'LEDtrackingParams', LEDtrackingParams,'frameIdx', file_frame_number,...
