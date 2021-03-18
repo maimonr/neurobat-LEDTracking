adf69db274e09961d79d163b57dc32c0b9b5f2f4
Maimon rose <maimon.rose@gmail.com>
2021-03-17 17:11:38 -0700

    allowed choosing ab averaging method

--- build_color_training_data.m
+++ build_color_training_data.m
@@ -4,8 +4,22 @@ varNames = gTruth.LabelData.Properties.VariableNames;
 trainingData= cell(size(gTruth.LabelData));
 frame_k = 1;
 minLum = 10;
-f = waitbar(0,'processing video');
-while hasFrame(v)
+ab_averaging_method = 'weightedMean';
+
+switch ab_averaging_method
+    case 'median'
+        lab_avg_func = @(lab) median(lab(:,2:3));
+    case 'weightedMean'
+        lab_avg_func = @(lab) mean(lab(:,2:3).*rescale(lab(:,1)));
+end
+
+
+waitH = waitbar(0,'processing video');
+
+emptyLabels = cellfun(@isempty,gTruth.LabelData.Variables);
+lastFrame = find(~all(emptyLabels,2),1,'last');
+
+while hasFrame(v) && frame_k <= lastFrame
     f = readFrame(v);
     for color_k = 1:length(varNames)
         if ~isempty(gTruth.LabelData{frame_k,color_k}{1})
@@ -14,7 +28,7 @@ while hasFrame(v)
             trainingData{frame_k,color_k} = f(roiIdx{2},roiIdx{1},:);
         end
     end
-    waitbar(frame_k/size(trainingData,1));
+    waitbar(frame_k/size(trainingData,1),waitH);
     frame_k = frame_k + 1;
 end
 
@@ -23,16 +37,17 @@ trainingData_lab = cell(size(trainingData));
 trainingData_lab(idx) = cellfun(@rgb2lab,trainingData(idx),'UniformOutput',false);
 trainingData_lab_thresh = cell(size(trainingData));
 for k = find(idx)'
-    frameData = reshape(trainingData_lab{k},[],3);
-    trainingData_lab_thresh{k} = frameData(frameData(:,1) > minLum,2:3);
+    trainingData_lab_thresh{k} = reshape(trainingData_lab{k},[],3);
+    lumIdx = trainingData_lab_thresh{k}(:,1) > minLum;
+    trainingData_lab_thresh{k}(lumIdx,:) = NaN;
 end
 
 color_training_label = {};
 color_training_mat = [];
 for color_k = 1:size(trainingData,2)
-    color_training_data = cellfun(@(x) median(x,1),trainingData_lab_thresh(:,color_k),'un',0);
-    idx = ~cellfun(@isempty,color_training_data);
-    color_training_mat = [color_training_mat; vertcat(color_training_data{idx})];
+    idx = ~cellfun(@isempty,trainingData_lab_thresh(:,color_k));
+    color_training_data = cellfun(@(lab) lab_avg_func(lab),trainingData_lab_thresh(idx,color_k),'un',0);
+    color_training_mat = [color_training_mat; vertcat(color_training_data{:})];
     color_training_label = [color_training_label;repmat(varNames(color_k),sum(idx),1)];
 end
 
