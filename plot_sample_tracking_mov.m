ffec1fde73b0b3a708c22b20efa7148b66a19729
Maimon rose <maimon.rose@gmail.com>
2021-04-26 17:56:01 -0700

    updated roi params

--- plot_sample_tracking_mov.m
+++ plot_sample_tracking_mov.m
@@ -20,8 +20,8 @@ switch pd.expType
         colorValues = {[1, 0, 0],[0, 1, 0],[1, 0.5, 0],[0, 1, 0.5],...
             [0, 0.5, 1],[0.5, 0, 1],[1, 0, 0.5],[0.5, 1, 0]};
     case 'sst'
-        colorKeys = {'re2','or2','ch2','gr2',...
-            'sp2','az2','bl2','vi2','rd2'};
+        colorKeys = {'re3','or3','ch3','gr3',...
+            'sp3','az3','bl3','vi3','rd3'};
         colorValues = {[1 0.2 0.2], [0.8 0.6 0], [0.6 0.8 0], [0.2 1 0.2],...
             [0 0.8 0.6], [0 0.6 0.8], [0.2 0.2 1], [0.6 0 0.8], [1 0 0]};
 end
@@ -29,15 +29,13 @@ colorValues = cellfun(@(rgb) rgb,colorValues,'un',0);
 color_name_table = containers.Map(colorKeys,colorValues);
 
 currentPos = pd.batPos(exp_date_str);
-bat_color_table = pd.get_bat_color_table(exp_date_str);
+[bat_color_table,~,ROI_rot] = pd.get_rec_day_info(exp_date_str);
 bat_color_table = containers.Map(bat_color_table.batNum,bat_color_table.color);
 
 switch pd.expType
     case 'adult_social'
-        ROI_rot = load('ROI_rot_adult_social.mat');
         fisheyeModel = load('fisheye_model_adult_social.mat');
     case 'sst'
-        ROI_rot = load('ROI_rot_sst.mat');
         fisheyeModel = load('fisheye_model_sst.mat');
 end
 
@@ -51,16 +49,16 @@ file_k = LEDTracks.fileIdx(frameIdx);
 v = VideoReader(fullfile(videoDir,LEDTracks.video_fNames(file_k).name));
 center_frame_k = LEDTracks.file_frame_number(frameIdx);
 
-frameOffset = round(v.FrameRate * tOffset);
+frameOffset_color = round(v.FrameRate * tOffset);
 
-frames = read(v,[max(1,center_frame_k-frameOffset)...
-    min(v.NumFrames,center_frame_k+frameOffset-1)]);
+frames = read(v,[max(1,center_frame_k-frameOffset_color)...
+    min(v.NumFrames,center_frame_k+frameOffset_color-1)]);
 nFrames = size(frames,4);
 
 if useOverlay
     video_data_dir = fullfile(pd.serverPath,'video_data');
     videoDir_IR = fullfile(pd.serverPath,exp_date_str,'video',[sessionType groupNum],'infrared');
-    overlayObjs = load('Regist_IR2color_objects_8132020','Rfixed','t');
+    overlayObjs = load('Regist_IR2color_sst','Rfixed','tAff');
     
     frame_ts_fname_color = fullfile(video_data_dir,...
         [exp_date_str '_color_frame_timestamps_info_' sessionType groupStr '.mat']);
@@ -80,10 +78,10 @@ if useOverlay
     [~,fname,ext] = fileparts(frame_ts_infrared.videoFNames{file_k});
     video_IR_fname = fullfile(videoDir_IR,[fname ext]);
     vIR = VideoReader(video_IR_fname);
-    frameOffset = round(vIR.FrameRate * tOffset);
+    frameOffset_IR = round(vIR.FrameRate * tOffset);
     
-    framesIR = read(vIR,[max(1,center_frame_k_IR-frameOffset)...
-        min(vIR.NumFrames,center_frame_k_IR+frameOffset-1)]);
+    framesIR = read(vIR,[max(1,center_frame_k_IR-frameOffset_IR)...
+        min(vIR.NumFrames,center_frame_k_IR+frameOffset_IR-1)]);
     
     if round(vIR.FrameRate) > round(v.FrameRate)
         n_ir_frames = size(framesIR,4);
@@ -91,12 +89,10 @@ if useOverlay
         framesIR = framesIR(:,:,:,subsampleIdx);
     end
     framesIR = squeeze(framesIR(:,:,1,:));
-    imSize(1,:) = size(frames,[1 2]);
-    imSize(2,:) = size(framesIR,[1 2]);
-    imSize = max(imSize);
+    imSize = overlayObjs.Rfixed.ImageSize;
     overlaid_video_data = zeros(imSize(1),imSize(2),3,nFrames,'uint8');
     for frame_k = 1:nFrames
-        coRegFrame = imwarp(framesIR(:,:,frame_k),overlayObjs.t,'OutputView',overlayObjs.Rfixed);
+        coRegFrame = imwarp(framesIR(:,:,frame_k),overlayObjs.tAff,'OutputView',overlayObjs.Rfixed);
         coRegFrame = imadjust(coRegFrame,[0 0.9],[0 0.5]);
         overlayFrame = frames(:,:,:,frame_k);
         overlaid_video_data(:,:,:,frame_k) = imadjust((repmat(coRegFrame,1,1,3) + overlayFrame)/2,[0 0.4]);
@@ -104,8 +100,8 @@ if useOverlay
     frames = overlaid_video_data;
 end
 imH = image(frames(:,:,:,1));
-ylim(ROI_rot.ylims+[0 100])
-xlim(ROI_rot.xlims+[0 100])
+ylim(ROI_rot.ylims+[-100 100])
+xlim(ROI_rot.xlims+[-100 100])
 axis square
 %%
 if saveMovie
@@ -122,7 +118,7 @@ for frame_k = 1:nFrames
     
     for batNum = batNums
         batPos = currentPos(batNum);
-        currentLoc = pd.pos2loc(batPos(frameIdx-frameOffset+frame_k+1,:),ROI_rot);
+        currentLoc = pd.pos2loc(batPos(frameIdx-frameOffset_color+frame_k+1,:),ROI_rot);
         if ~any(isnan(currentLoc))
             colorStr = bat_color_table(batNum);
             batColor = 256*color_name_table(colorStr);
@@ -130,12 +126,12 @@ for frame_k = 1:nFrames
                 batNum,'Color',batColor,'TextBoxOpacity',0,'TextColor',batColor);
         end
     end
-    currentFrame = imrotate(currentFrame,-ROI_rot.deg,'bilinear');
+    currentFrame = imrotate(currentFrame,ROI_rot.deg,'bilinear');
     imH.CData = currentFrame;
     if saveMovie
        writeVideo(vWrite,currentFrame(cropIdx{1}(1):cropIdx{1}(2),cropIdx{2}(1):cropIdx{2}(2),:,:)); 
     end
-    pause(0.05)
+    pause(0.1)
 end
 
 if saveMovie
